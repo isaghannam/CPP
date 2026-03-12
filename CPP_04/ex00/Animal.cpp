8d00899b2c7a654efd58158957b9755ebde2dced
Isadora Ghannam <isa.gha@gmail.com>
2026-03-12 17:54:40 -0300

    add: partial CPP04

---
+++ CPP_04/ex00/Animal.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ighannam <ighannam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 16:44:40 by ighannam          #+#    #+#             */
+/*   Updated: 2026/03/12 16:45:35 by ighannam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal()
+{
+    
+}
+
+Animal::Animal(std::string type)
+{
+    
+}
+
+Animal::~Animal()
+{
+    
+}
+
+Animal& Animal::operator+(const Animal& other)
+{
+    
+}
+
+Animal::Animal(const Animal& other)
+{
+    
+}
+
+void Animal::makeSound()
+{
+    
+}
---
+++ CPP_04/ex00/Animal.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ighannam <ighannam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 16:24:14 by ighannam          #+#    #+#             */
+/*   Updated: 2026/03/12 16:44:31 by ighannam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+    protected:
+        std::string type;
+    public:
+        Animal();
+        Animal(std::string type);
+        ~Animal();
+        Animal& operator+(const Animal& other);
+        Animal(const Animal& other);
+        void makeSound();
+};
+
+
+
+#endif
